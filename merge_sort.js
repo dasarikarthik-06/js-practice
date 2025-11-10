293621f1966aa200494933731c9834365b77c367
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-11-10 18:19:21 +0530

    Update merge_sort.js

--- merge_sort.js
+++ merge_sort.js
@@ -1,6 +1,13 @@
+const merge = (data) => {
+   
+}
+
 const mergeSort = (data) => {
-return data;
+  const leftHalf = data.slice(0, data.length / 2)
+  const rightHalf  = data.slice(data.length / 2);
+  return rightHalf.concat(leftHalf);
 }
 const data = [9,4,2,2,1];
+console.log(data)
 const sortedData = mergeSort(data)
 console.log(sortedData);
