17f15455a14dc479e8f5106ef7628eda781695d7
Dasari Karthik <karthikdasari.1682@gmail.com>
2025-11-30 20:36:54 +0530

    Add DFA for divisibility by 2 or 3
    Implemented a DFA to check membership for strings divisible by 2 or 3.

--- automata/divisibleByTwoOrThree.js
+++ automata/divisibleByTwoOrThree.js
@@ -1,1 +1,20 @@
-//under development
+const dfa = {
+  q0: { 0: "q1", isFinal: true },
+  q1: { 0: "q2", isFinal: false },
+  q2: { 0: "q3", isFinal: true },
+  q3: { 0: "q4", isFinal: true },
+  q4: { 0: "q5", isFinal: true },
+  q5: { 0: "q0", isFinal: false },
+};
+
+export const isMember = (string) => {
+  let currentState = "q0";
+  currentState = [..." ".repeat(string.length)].reduce(
+    (previousState, currentState) => {
+      currentState = dfa[previousState][0];
+      return currentState;
+    },
+    currentState,
+  );
+  return dfa[currentState].isFinal;
+};
